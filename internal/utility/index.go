a3eb394feb6053890ecd450aa9bdb83653b48591
luckyAkbar <luckyakbar1509@gmail.com>
2022-04-07 21:04:20 +0700

    feature/add-report-log

--- internal/utility/index.go
+++ internal/utility/index.go
@@ -1,12 +1,33 @@
 package utility
 
-type Utility struct{}
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+)
+
+type Utility struct{
+	logFileTarget *os.File
+}
 
 const IMAGE_URL = "https://siakadu.unila.ac.id/uploads/fotomhs/"
 
 var POSSIBLE_IMAGE_EXT []string = []string{".jpg", ".jpeg", ".png"}
 var BASE_IMAGE_STORAGE = "./result/images/"
+var LOG_FILE_LOCATION = fmt.Sprintf("./report/%s.log", time.Now().Local().String())
 
 func New() *Utility {
-	return &Utility{}
+	logFile, err := os.OpenFile(LOG_FILE_LOCATION, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+
+	if err != nil {
+		log.Println("Failed to initialize logfile")
+		log.Println(err.Error())
+
+		os.Exit(-1)
+	}
+
+	return &Utility{
+		logFileTarget: logFile,
+	}
 }
