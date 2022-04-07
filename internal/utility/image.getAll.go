5e9edc1551cae2e71effffa6aefc10151bf45818
luckyAkbar <luckyakbar1509@gmail.com>
2022-04-08 05:52:04 +0700

    add optimizer: skipper when sequentially failed to get image in get images all command

--- internal/utility/image.getAll.go
+++ internal/utility/image.getAll.go
@@ -2,11 +2,15 @@ package utility
 
 import "time"
 
-func (u *Utility) GetAllProfileImage(delay int) {
+func (u *Utility) GetAllProfileImage(delay, maxSeqFailure int) {
 	i := NewIncrementor("0000000000", "9999999999")
+	i.SetMaxSequentialFailure(maxSeqFailure)
 
 	for !i.IsMaxReached {
-		u.GetImageFromNPM(i.GetCurrent())
+		if err := u.GetImageFromNPM(i.GetCurrent()); err != nil {
+			i.IncrementSeqFailureCount()
+		}
+
 		i.Next()
 
 		time.Sleep(time.Duration(delay) * time.Second)
