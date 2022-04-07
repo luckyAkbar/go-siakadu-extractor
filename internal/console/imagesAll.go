7f93506b10770ca6e07dbaa7346716e4bc249562
luckyAkbar <luckyakbar1509@gmail.com>
2022-04-08 05:49:03 +0700

    add flag max-seq-failure with default to 10

--- internal/console/imagesAll.go
+++ internal/console/imagesAll.go
@@ -18,4 +18,5 @@ var getAllImagesCmd = &cobra.Command{
 func init() {
 	imagesCmd.AddCommand(getAllImagesCmd)
 	getAllImagesCmd.PersistentFlags().Int("request-delay-sec", 0, "Make every request delayed as input (ms)")
+	getAllImagesCmd.PersistentFlags().Int("max-seq-failure", 10, "Add division skipping when sequentially absent crawling is failed / not found")
 }
