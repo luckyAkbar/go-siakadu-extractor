a7c4e8b22eee7ecf007fd166bf8fa4188ae7f0ef
luckyAkbar <luckyakbar1509@gmail.com>
2022-04-14 06:12:35 +0700

    register new flag: start and to

--- internal/console/imagesAll.go
+++ internal/console/imagesAll.go
@@ -7,16 +7,18 @@ import (
 )
 
 var getAllImagesCmd = &cobra.Command{
-	Use: "all",
-	Short: "Get all public profile picture",
-	Long: "Get all public profile picture from all posible NPM",
+	Use:     "all",
+	Short:   "Get all public profile picture",
+	Long:    "Get all public profile picture from all posible NPM",
 	Example: "images all",
 	Version: "1",
-	Run: feature.GetAllProfileImage,
+	Run:     feature.GetAllProfileImage,
 }
 
 func init() {
 	imagesCmd.AddCommand(getAllImagesCmd)
 	getAllImagesCmd.PersistentFlags().Int("request-delay-sec", 0, "Make every request delayed as input (ms)")
 	getAllImagesCmd.PersistentFlags().Int("max-seq-failure", 10, "skip and increment division if sequentially failed to get images from given NPM when sequentially absent crawling is failed / not found")
+	getAllImagesCmd.PersistentFlags().String("start", "0000000000", "Set starting point for full images crawling. Default to: '0000000000'")
+	getAllImagesCmd.PersistentFlags().String("to", "9999999999", "Set final point for full images crawling. Default to: '9999999999'")
 }
