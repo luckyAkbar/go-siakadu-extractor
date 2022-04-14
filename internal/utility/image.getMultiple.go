411c3053164fd7bf478de72ef4bb437e916efd72
luckyAkbar <luckyakbar1509@gmail.com>
2022-04-14 08:05:13 +0700

    add feature to enabling request optimizer

--- internal/utility/image.getMultiple.go
+++ internal/utility/image.getMultiple.go
@@ -5,11 +5,21 @@ import (
 	"time"
 )
 
-func (u *Utility) GetImageFromNPMRange(start, to string, delay int) {
+func (u *Utility) GetImageFromNPMRange(start, to string, delay, requestOptimizer int) {
 	i := NewIncrementor(start, to)
+	i.SetMaxSequentialFailure(10)
+
+	if requestOptimizer != 0 {
+		i.SetMaxSequentialFailure(requestOptimizer)
+	}
 
 	for !i.IsMaxReached {
-		u.GetImageFromNPM(i.GetCurrent())
+		err := u.GetImageFromNPM(i.GetCurrent())
+
+		if err != nil && requestOptimizer != 0 {
+			i.IncrementSeqFailureCount()
+		}
+
 		i.Next()
 
 		time.Sleep(time.Duration(delay) * time.Second)
