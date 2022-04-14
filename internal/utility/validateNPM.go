fa59b5a7ca30829eaf89656f2ffc18c39d577b16
luckyAkbar <luckyakbar1509@gmail.com>
2022-04-14 07:16:41 +0700

    add new function: EnsureNoBackwardNPM

--- internal/utility/validateNPM.go
+++ internal/utility/validateNPM.go
@@ -18,3 +18,31 @@ func ValidateNPM(NPM string) error {
 
 	return nil
 }
+
+func EnsureNoBackwardNPM(first, second string) error {
+	start, err := strconv.Atoi(first)
+
+	if err != nil {
+		return fmt.Errorf("NPM value is invalid: %s", first)
+	}
+
+	to, err := strconv.Atoi(second)
+
+	if err != nil {
+		return fmt.Errorf("NPM value is invalid: %s", first)
+	}
+
+	if start > to {
+		return fmt.Errorf("backward NPM value detected: %s > %s", first, second)
+	}
+
+	if err := ValidateNPM(first); err != nil {
+		return fmt.Errorf("invalid value for NPM: %s", first)
+	}
+
+	if err := ValidateNPM(second); err != nil {
+		return fmt.Errorf("invalid value for NPM: %s", second)
+	}
+
+	return nil
+}
