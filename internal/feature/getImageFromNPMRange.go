eb03fea4a16bad926a56a2eeebe41afeab9f6c34
luckyAkbar <luckyakbar1509@gmail.com>
2022-04-14 08:04:13 +0700

    add feature to enabling request optimizer

--- internal/feature/getImageFromNPMRange.go
+++ internal/feature/getImageFromNPMRange.go
@@ -9,8 +9,15 @@ import (
 )
 
 func GetImageFromNPMRange(cmd *cobra.Command, args []string) {
+	var requestOptimizer int = 0
 	util := utility.New()
 	delay := util.GetRequestDelaySecFlag(cmd)
+
+	if val := util.GetIntFlagValueByName(cmd, "use-image-req-optimizer"); val != 0 {
+		log.Printf("Image request optimizer enabled with value: %d\n", val)
+
+		requestOptimizer = val
+	}
 
 	if err := utility.RequiredArgsNum(args, 2); err != nil {
 		log.Print(err.Error())
@@ -26,6 +33,6 @@ func GetImageFromNPMRange(cmd *cobra.Command, args []string) {
 		}
 	}
 
-	util.GetImageFromNPMRange(args[0], args[1], delay)
+	util.GetImageFromNPMRange(args[0], args[1], delay, requestOptimizer)
 	os.Exit(0)
 }
